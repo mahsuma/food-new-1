6d0411cb16a09c8a0306d4b6f40ff7a48bc8288d
Mahsuma <74092449+mahsuma@users.noreply.github.com>
2021-03-25 18:13:14 +0430

    food app

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    
+      <Text> hello form react native
+
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+
+}
+
+const styles = StyleSheet.create({
+
+
+
+  container: {
+    flex: 1,
+    
+  },
+});
